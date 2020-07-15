aab57954495e6ef9e1153fdf4cbcd77ae117050f
ArthurToledo <arthur.toledo741@gmail.com>
2020-07-15 13:58:50 -0300

    Import new modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CandidatesModule } from './candidates/candidates.module';
+import { TestsModule } from './testes/tests.module';
+import { CandidatesTestsModule } from './candidates-tests/candidates-tests.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: 'Sa_teste1234',
+      database: 'testNest',
+      autoLoadModels: true,
+      synchronize: true,
+      dialectOptions: {
+        trustServerCertificate: true
+      },
+      define: {
+        timestamps: true
+      },
+    }),
+    CandidatesModule,
+    TestsModule,
+    CandidatesTestsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
